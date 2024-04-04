97476c2a179bb6f8b7d6ad412fdc3dba6fb61103
SteveR <steve17rouse@gmail.com>
2024-04-03 20:38:09 -0600

    created about us cards

---
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,74 @@
+<div class="ui four cards">
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="4"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="2"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="3"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="4"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="3"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="3"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="4"></div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img/>
+    </div>
+    <div class="extra">
+      Rating:
+      <div class="ui star rating" data-rating="4"></div>
+    </div>
+  </div>
+</div>
