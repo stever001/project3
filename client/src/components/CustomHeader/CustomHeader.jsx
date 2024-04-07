d343b587e5ddc0b39d8a0456230dfcac77d07804
Dougy Fresh <dougrouseltw@gmail.com>
2024-04-07 16:49:05 -0600

    added content to Header, adjusted css for header

--- client/src/components/CustomHeader/CustomHeader.jsx
+++ client/src/components/CustomHeader/CustomHeader.jsx
@@ -1,17 +1,24 @@
 
 import React from 'react';
 import './CustomHeader.css'; // Importing Header-specific styles
-
 import Navigation from '../Navigation/Navigation';
 
 function CustomHeader() {
+  const logoUrl = process.env.PUBLIC_URL + '/assets/trans-logo.png'; // Get the absolute URL for the logo
+
   return (
     <div className="CustomHeader-wrapper">
-      <h1>HealthSync</h1>
+      <div className="header-top"> {/* Container for the logo and the title */}
+        <div className="header-logo">
+          <a href="/">
+            <img src={logoUrl} alt="HealthSync Logo" />
+          </a>
+        </div>
+        <h1 className="header-title">HealthSync</h1>
+      </div>
       <Navigation />
     </div>
   );
 }
 
 export default CustomHeader;
-
