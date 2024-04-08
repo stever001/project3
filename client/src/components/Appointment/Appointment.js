026e95d759395f6d875755253896e30b205a9df6
RMP <ruthiepina@gmail.com>
2024-04-08 14:43:26 -0600

    fixed styling on schedule an appt page! Not cut off anymore

--- client/src/components/Appointment/Appointment.js
+++ client/src/components/Appointment/Appointment.js
@@ -4,9 +4,11 @@ import "./Appointment.css"; // Importing CSS
 
 function Appointment() {
    return (
-      <div className="contact-container">
-         <h2 className="contact-heading">Schedule an Appointment</h2> {/* Added class here */}
-         <AppointmentFormWrapper /> {/* Using ContactFormWrapper here */}
+      <div className="appointment-wrapper">
+         <div className="appointment-container">
+            <h2 className="appointment-heading">Schedule an Appointment</h2> {/* Added class here */}
+            <AppointmentFormWrapper /> {/* Using ContactFormWrapper here */}
+         </div>
       </div>
    );
 }
