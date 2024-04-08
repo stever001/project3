fe0e413892518ef4f623b69bde02a0558df078d5
Aaron Cendejas <accenaar0@gmail.com>
2024-04-07 18:28:15 -0600

    fixed front end for the login/signup page

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import CustomHeader from './components/CustomHeader/CustomHeader';
 import BackgroundSlider from './components/BackgroundSlider/BackgroundSlider';
 import AboutUs from './components/AboutUs/AboutUs';
-import LoginForm from './components/auth/LoginForm';
+import LoginForm from './components/auth/LoginForm'; // Corrected import path
 import Contact from './components/Contact/Contact';
-import SignupForm from './components/auth/SignupForm';
+// import SignupForm from './components/auth/SignupForm';
 import HomeComponent from './components/HomeComponent/HomeComponent';
 import './App.css';
 
@@ -21,7 +21,7 @@ function App() {
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/signup" element={<SignupForm />} />
+        {/* <Route path="/signup" element={<SignupForm />} /> */} {/* Uncomment or remove this line as needed */}
         {/* Add other routes as needed */}
       
       </Routes>
